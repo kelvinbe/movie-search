cece9dd0e09c34113b286dfd8a11c38530fe4242
kelvin beno <kelvinbeno526@gmail.com>
2022-04-25 19:02:01 +0300

    updates to code

--- src/components/Movieform.js
+++ src/components/Movieform.js
@@ -1,6 +1,66 @@
-import React from 'react'
+import { use } from 'chai'
+import { setState } from 'expect'
+import React, {useState} from 'react'
+import Movieslist from './Movieslist'
+import Search from './Search'
 
-function Movieform() {
+function Movieform(props) {
+
+    const [movies, setMovies] = useState([])
+    const [name, setName] = useState('')
+    const [ratings, setRating] = useState(null)
+    const [duration, setDuration] = useState('')
+    const [error, setError] = useState(false)
+
+    const handleName = (e) => {
+      console.log(e.target.value)
+
+      setName(e.target.value)
+    }
+    const handleRating = (e) => {
+      console.log(e.target.value)
+
+      setRating(e.target.value)
+      
+    }
+    const handleDuration = (e) => {
+      console.log(e.target.value) 
+      if(e.target.value.length > 0){
+        setError(false)
+      }
+      setDuration(e.target.value)
+      
+    }
+
+    const handleAddMovie = () => {
+      if(!duration.includes('m') || duration.includes('h')){
+          console.log('duration', duration)
+          
+          
+          setError(true)
+      }
+
+      if(duration.includes('m')){
+        const numDuration = parseInt(duration)
+        const hrs = Math.floor(numDuration / 60)
+        hrs.toString()
+        return setDuration(hrs)
+        
+      }
+      console.log('duration', duration)
+
+        const newMovie = {
+          'movieName': name,
+          'Rating': ratings,
+          'duration': duration
+        }
+        console.log(newMovie)
+       setMovies([...movies, newMovie])
+       console.log(movies)
+
+    }
+
+const Form = () => {
 
   return (
     <section>
@@ -13,6 +73,8 @@ function Movieform() {
               id='name'
               placeholder='Enter Movie Name'
               data-testid='nameInput'
+              onChange={handleName}
+              value={name}
             />
           </div>
           <div className='layout-column mb-15'>
@@ -22,6 +84,8 @@ function Movieform() {
               id='ratings'
               placeholder='Enter Rating on a scale of 1 to 100'
               data-testid='ratingsInput'
+              onChange={handleRating}
+              value={ratings}
             />
           </div>
           <div className='layout-column mb-30'>
@@ -31,20 +95,24 @@ function Movieform() {
               id='duration'
               placeholder='Enter duration in hours or minutes'
               data-testid='durationInput'
+              onChange={handleDuration}
+              value={duration}
             />
           </div>
           {/* Use this div when time format is invalid */}
-          {/* <div 
+         {error &&<div 
             className='alert error mb-30'
             data-testid='alert'
           >
             Please specify time in hours or minutes (e.g. 2.5h or 150m)
-          </div>  */}
+          </div> }
           <div className='layout-row justify-content-end'>
             <button 
               type='submit'
               className='mx-0'
               data-testid='addButton'
+              onClick={handleAddMovie}
+              
             >
               Add Movie
             </button>
@@ -53,6 +121,29 @@ function Movieform() {
       </div> 
     </section>
   )
+
+}
+const title = 'Favorite Movie Directory'
+    
+
+    return (
+      <div>
+      <h8k-navbar header={ title } />
+      <div className='layout-row justify-content-center mt-100'>
+        <div className='w-30 mr-75'>
+          {Form()}
+        </div>
+        <div className='layout-column w-30'>
+          <Search />
+          <Movieslist movies={movies} /> 
+          <div data-testid='noResult'>
+            <h3 className='text-center'>No Results Found</h3>
+          </div>
+        </div>
+      </div> 
+    </div>
+    )
+  
 }
 
 export default Movieform
